ddc7e373106ae2ce6a306bbbc561eedab553db19
KeniaKodigo <kenia.paz@kodigo.org>
2023-11-21 20:42:49 -0600

    principio inversion de dependencia

---
+++ SOLID/inversionDependencia.php
@@ -0,0 +1,15 @@
+<?php
+
+class Mysql{
+    //host, puerto, bd, gestor (mysql)
+}
+
+
+
+class Procesar{
+    //Mysql
+}
+
+
+
+?>
--- SOLID/inversionDependency.php
+++ SOLID/inversionDependency.php
@@ -1,11 +1,13 @@
 <?php
 
+#clase bajo nivel
 class ReporteFPDF{
     public function exportar(){
         return "Exportando el documento en FPDF";
     }
 }
 
+#alto nivel
 class Inventario{
     public function procesarProductos(){
         //instancia (creando un objeto)
@@ -15,6 +17,45 @@ class Inventario{
 }
 
 
+#deben de depender de abstracciones
 
+interface tipoPDF{
+    public function generarDocumento();
+}
+
+class FPDF implements tipoPDF{
+    public function generarDocumento()
+    {
+        return "estas trabajando con la libreria fpdf";
+    }
+}
+
+class DOMPDF implements tipoPDF{
+    public function generarDocumento()
+    {
+        return "estas trabajando con la libreria dompdf";
+    }
+}
+
+class MPDF implements tipoPDF{
+    public function generarDocumento()
+    {
+        return "estas trabajando con la libreria mpdf";
+    }
+}
+
+class GestionInventario{
+
+    //clase fpdf o dompdf o mpdf
+    public function procesarProductos(tipoPDF $pdf){
+        return $pdf->generarDocumento();
+    }
+}
+
+$inventario = new GestionInventario();
+$inventario->procesarProductos(new FPDF); //estas trabajando con la libreria fpdf
+
+$inventario2 = new GestionInventario();
+$inventario2->procesarProductos(new DOMPDF); //estas trabajando con la libreria dompdf
 
 ?>
